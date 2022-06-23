dbb4e9d3c2e77adff3b993a2c64b7042469193b2
LuizBlading <luizfelipe1342@yahoo.com.br>
2022-06-23 18:51:44 -0300

    Melhorando Cards e aplicando responsividade

--- src/app/pokemons/pokemons.page.ts
+++ src/app/pokemons/pokemons.page.ts
@@ -13,8 +13,10 @@ export class PokemonsPage implements OnInit {
   public pokemonObjResponse = [];
   public namePokemon: String;
   public isRequest: Boolean;
-  public indices = [1, 2, 3, 4, 5];
+  public indices = [1,2,3,4,5,6];
   public imgPokemon: any = [];
+  public hasType1: Boolean;
+  public hasType2: Boolean;
 
   constructor(
     private pokemonService: PokemonAPI
@@ -28,11 +30,22 @@ export class PokemonsPage implements OnInit {
       for(let indice of this.indices ){
         const response = await this.pokemonService.getPokemonById(indice);
 
-        
         if (response) {
           this.imgPokemon = response.sprites.front_default;
           this.pokemonObjResponse.push(response);
-          this.isRequest = true;
+          this.isRequest = true;    
+          
+          if(response.types[0] != null){
+            this.hasType1 = true;
+          } else {
+            this.hasType1 = false;
+          }
+
+          if(response.types[1] != null){
+            this.hasType2 = true;
+          } else {
+            this.hasType2 = false;
+          }
         }
       }
     } catch (e) {
